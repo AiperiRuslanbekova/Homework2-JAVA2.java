d43ec057afc1cce9f18c50810c820ae5fad1cd5c
Aiperi <aruslanbekova1@gmail.com>
2021-02-10 16:05:35 +0600

    Initial commit

---
+++ src/com/company/Transport.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Transport {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
